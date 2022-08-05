65c6b880689e59bb8bb530af1dcf32e63622d342
Endri Dibra <104068619+EndriDibra@users.noreply.github.com>
2022-08-05 04:10:39 +0300

    Add files via upload

---
+++ speech_to_text.py
@@ -0,0 +1,29 @@
+# Below i use the required libraries
+import speech_recognition as sp
+
+recognizer = sp.Recognizer()
+
+while True:
+
+    try:
+
+        # Using the required method to use computer's microphone
+        # to take user's speech from audio
+
+        with sp.Microphone() as mic:
+
+            recognizer.adjust_for_ambient_noise(mic, duration=0.5)
+            audio = recognizer.listen(mic)
+
+            # Converting my speech to text
+            text = recognizer.recognize_google(audio)
+            text = text.lower()
+
+            print("You said : ", text)
+
+    except sp.UnknownValueError():
+
+        # continues till an exception occurs
+        recognizer = sp.Recognizer()
+
+        continue
